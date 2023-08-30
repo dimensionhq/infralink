9ee972c01e9dd48da79750b9f121957cb91d53a7
GitHub Action <action@github.com>
2023-08-30 16:44:03 +0000

    Auto format code

--- cli/src/constants/commands.rs
+++ cli/src/constants/commands.rs
@@ -1,3 +1,4 @@
 use once_cell::sync::Lazy;
 
-pub static COMMANDS_LIST: Lazy<Vec<String>> = Lazy::new(|| vec![String::from("init"), String::from("build")]);
+pub static COMMANDS_LIST: Lazy<Vec<String>> =
+    Lazy::new(|| vec![String::from("init"), String::from("build")]);
