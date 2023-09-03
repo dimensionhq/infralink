3433774773151db440fd1c504b7c4348d373c93d
Tejas Ravishankar <tejas@dimension.dev>
2023-09-03 19:19:48 +0400

    feat(apis/pricing): rate limiting

--- apis/pricing/src/main.rs
+++ apis/pricing/src/main.rs
@@ -3,6 +3,7 @@ pub mod models;
 pub mod routes;
 pub mod validator;
 
+use actix_governor::{Governor, GovernorConfigBuilder};
 use actix_web::{web::Data, App, HttpServer};
 use routes::{
     external_data_transfer, inter_region_data_transfer, on_demand, spot, spot_forecast, storage,
@@ -10,19 +11,25 @@ use routes::{
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Create env_logger with log level set to INFO
+    // Per-minute governor configuration
+    let per_minute_conf = GovernorConfigBuilder::default()
+        .per_millisecond(4000)
+        .burst_size(4)
+        .finish()
+        .unwrap();
+
+    // Initialize logging
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    // Database URL
+    // Database connection
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    // Create connection
     let pool = db::create_pool(&database_url).await;
 
-    // Start HTTP server
+    // HTTP server
     HttpServer::new(move || {
         App::new()
             .wrap(actix_web::middleware::Logger::default())
+            .wrap(Governor::new(&per_minute_conf))
             .app_data(Data::new(pool.clone()))
             .service(on_demand)
             .service(spot)
